cc6625cd9d3fe802caa41fd6fda25df376a35f5f
akshaysi05 <akshaysingh8804@gmail.com>
2023-10-02 17:04:42 +0530

    Run All Test With TestNg.XML File

---
+++ TestNgTutorial/src/test/java/testNgPack/Test3.java
@@ -0,0 +1,19 @@
+package testNgPack;
+
+import org.testng.annotations.Test;
+
+public class Test3 {
+
+	@Test
+	public void WebLoginCar() {
+       System.out.println("WebLoginCar");
+	}
+    @Test
+	public void ApiLoginCar() {
+      System.out.println("ApiLoginCar");
+	}
+   @Test
+	public void MobileLoginCar() {
+     System.out.println("MobileLoginCar");
+	}
+}
---
+++ TestNgTutorial/src/test/java/testNgPack/Test4.java
@@ -0,0 +1,19 @@
+package testNgPack;
+
+import org.testng.annotations.Test;
+
+public class Test4 {
+
+	@Test
+	public void WebLoginHome() {
+       System.out.println("WebLoginHome");
+	}
+    @Test
+	public void ApiLoginHome() {
+      System.out.println("ApiLoginHome");
+	}
+   @Test
+	public void MobileLoginHome() {
+     System.out.println("MobileLoginHome");
+	}
+}

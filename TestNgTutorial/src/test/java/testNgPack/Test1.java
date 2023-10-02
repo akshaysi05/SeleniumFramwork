54dcec4e96a7e07b3cb6ba21585b0894a65786cd
akshaysi05 <akshaysingh8804@gmail.com>
2023-10-02 16:31:46 +0530

    Run TestNg And XML Files

---
+++ TestNgTutorial/src/test/java/testNgPack/Test1.java
@@ -0,0 +1,17 @@
+package testNgPack;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void Demo() {
+		
+		System.out.println("Test First");
+	}
+	@Test
+	public void demo2() {
+		System.out.println("Test Seconds");
+	}
+
+}
---
+++ TestNgTutorial/src/test/java/testNgPack/Test2.java
@@ -0,0 +1,12 @@
+package testNgPack;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	@Test
+	public void Demo3() {
+		System.out.println("test Three ");
+	}
+
+}
